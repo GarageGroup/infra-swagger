304c512763145a1b9b732d3aed03ccbf9bf7b266
pmosk <pm@ggroupp.ru>
2023-03-09 14:54:33 +0400

    Improve Swagger UI

--- src/Swagger.Core/SwaggerApplicationBuilderExtensions.cs
+++ src/Swagger.Core/SwaggerApplicationBuilderExtensions.cs
@@ -12,7 +12,9 @@ namespace Microsoft.AspNetCore.Builder;
 
 public static class SwaggerApplicationBuilderExtensions
 {
-    private const string RouteTemplate = "/swagger/{documentName}/swagger.json";
+    private const string RouteTemplateJson = "/swagger/{documentName}/swagger.json";
+
+    private const string RouteTemplateYaml = "/swagger/{documentName}/swagger.yaml";
 
     public static TApplicationBuilder UseSwagger<TApplicationBuilder>(
         this TApplicationBuilder applicationBuilder, Func<IServiceProvider, ISwaggerDocumentProvider> swaggerProviderResolver)
@@ -21,27 +23,35 @@ public static class SwaggerApplicationBuilderExtensions
         ArgumentNullException.ThrowIfNull(applicationBuilder);
         ArgumentNullException.ThrowIfNull(swaggerProviderResolver);
 
-        var route = new RouteBuilder(applicationBuilder).MapVerb(HttpMethod.Get.Method, RouteTemplate, InnerInvokeAsync).Build();
+        var route = new RouteBuilder(applicationBuilder)
+            .MapVerb(HttpMethod.Get.Method, RouteTemplateJson, InnerInvokeJsonAsync)
+            .MapVerb(HttpMethod.Get.Method, RouteTemplateYaml, InnerInvokeYamlAsync)
+            .Build();
 
         _ = applicationBuilder.UseRouter(route);
         return applicationBuilder;
 
-        Task InnerInvokeAsync(HttpContext context)
+        Task InnerInvokeJsonAsync(HttpContext context)
             =>
-            context.InvokeAsync(swaggerProviderResolver);
+            context.InvokeAsync(swaggerProviderResolver, OpenApiFormat.Json);
+
+        Task InnerInvokeYamlAsync(HttpContext context)
+            =>
+            context.InvokeAsync(swaggerProviderResolver, OpenApiFormat.Yaml);
     }
 
-    private static async Task InvokeAsync(this HttpContext httpContext, Func<IServiceProvider, ISwaggerDocumentProvider> providerResolver)
+    private static async Task InvokeAsync(
+        this HttpContext httpContext, Func<IServiceProvider, ISwaggerDocumentProvider> providerResolver, OpenApiFormat format)
     {
         var documentName = httpContext.GetDocumentName() ?? string.Empty;
 
         var swaggerHubProvider = providerResolver.Invoke(httpContext.RequestServices);
         var document = await swaggerHubProvider.GetDocumentAsync(documentName, httpContext.RequestAborted).ConfigureAwait(false);
 
-        var json = document.Serialize(OpenApiSpecVersion.OpenApi3_0, OpenApiFormat.Json);
+        var json = document.Serialize(OpenApiSpecVersion.OpenApi3_0, format);
 
         httpContext.Response.StatusCode = StatusCodes.Status200OK;
-        httpContext.Response.Headers["Content-Type"] = MediaTypeNames.Application.Json;
+        httpContext.Response.Headers["Content-Type"] = format is OpenApiFormat.Json ? MediaTypeNames.Application.Json : "application/yaml";
 
         await httpContext.Response.WriteAsync(json, httpContext.RequestAborted).ConfigureAwait(false);
     }
